d4ea9be2ce14c842ce1a338d3b0bf09cb0f50ae2
jasperdelaat <j.delaat1@student.avans.nl>
2015-05-28 10:50:42 +0200

    Loadout, Inventory, Ninja

---
+++ WPFNinjaV2/Model/DummyInventoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNinjaV2.Model
+{
+    public class DummyInventoryRepository : IInventory
+    {
+        public List<Gear> ToList()
+        {
+            var loadout = new List<Gear>();
+
+            return loadout;
+        }
+    }
+}
---
+++ WPFNinjaV2/Model/DummyLoadoutRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNinjaV2.Model
+{
+    public class DummyLoadoutRepository : ILoadout
+    {
+        public List<Gear> ToList()
+        {
+            var loadout = new List<Gear>();
+
+            return loadout;
+        }
+
+    }
+}
