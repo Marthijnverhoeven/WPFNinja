9efc4e519cfb6da2a51f7eb1d76534472244ecef
Marthijn Verhoeven <mmamhver@student.avans.nl>
2015-05-28 10:44:58 +0200

    equipment

---
+++ WPFNinjaV2/Model/EquipmentItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNinjaV2.Model
+{
+    public class EquipmentItem
+    {
+        public static int HEAD = 0;
+        public static int SHOULDERS = 1;
+        public static int CHEST = 2;
+        public static int BELT = 3;
+        public static int LEGS = 4;
+        public static int BOOTS = 5;
+
+        public int id { get; set; }
+        public int type { get; set; }
+        public int intelligence { get; set; }
+        public int strength { get; set; }
+        public int agility { get; set; }
+        public int price { get; set; }
+    }
+}
