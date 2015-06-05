ad61dcf48323b08ee2bd68af152fc17595fa11de
jasperdelaat <j.delaat1@student.avans.nl>
2015-06-05 13:33:12 +0200

    Dingen

---
+++ WPFNinjaV2/Model/DummyCurrentLoadoutRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNinjaV2.Model
+{
+    public class DummyCurrentLoadoutRepository : ICurrentLoadout
+    {
+        public List<EquipmentItem> ToList()
+        {
+            var items = new List<EquipmentItem>();
+            items.Add(new EquipmentItem { id = 1, type = EquipmentItem.HEAD, intelligence = 100, strength = 100, agility = 100, price = 100 });
+
+            return items;
+        }
+    }
+}
